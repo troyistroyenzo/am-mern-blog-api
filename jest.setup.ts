cf644509bcb2e932709ccb20edff6eb7eec90c3b
vrfdivino <vrfdivino@gmail.com>
2024-09-15 21:15:18 +0800

    chore: clean

--- jest.setup.ts
+++
@@ -1,9 +0,0 @@
-import { jest } from "@jest/globals";
-
-jest.mock("@/lib/mongodb", () => ({
-  connectToDatabase: jest.fn(),
-}));
-
-jest.mock("query-string", () => ({
-  parseUrl: jest.fn(() => ({ url: "", query: {} })),
-}));
